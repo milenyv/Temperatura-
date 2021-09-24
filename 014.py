9469e687b7ff77463c5134ed547396dbe7f0a05f
mileny <82714273+milenyv@users.noreply.github.com>
2021-09-24 19:05:41 -0300

    Add files via upload

---
+++ 014.py
@@ -0,0 +1,5 @@
+c = float(input("informe a temperatura em c°? "))
+f = 9 * c / 5 + 32
+K = c + 273.15
+print("A temperatura de {}°c corresponde a {}°F!".format(c, f))
+print("A temperatura em {}°c corresponde a {}K ".format(c, K,))
